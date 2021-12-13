a8cbbff0093f3632b0baa9d9d7abf157bfc8ca80
tasha984288 <tasha984288@gmail.com>
2021-12-13 14:42:20 -0500

    python classes

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Class square that defines a square"""
+
+
+class Square:
+    """An empty block"""
+    pass
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""""""Class Square that defines a square object""""""
+
+
+class Square:
+    def __init__(self, size):
+        """Initialize method that stores the size of the square
+
+        Args:
+            param1 (int): size of the square
+        """
+        self.__size = size
